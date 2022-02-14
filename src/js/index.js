5c912e04c0961a981b3b029e41bd7861f6eb7632
Jiro Koshio <koshio@web-shizuoka.net>
2022-02-14 20:54:28 +0900

    フィードバック3

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,17 @@ $(window).on("scroll", function () {
     }
   });
 });
+
+/* ===================================================
+// Smooth Scroll
+==================================================== */
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let speed = 500;
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
